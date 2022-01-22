5cec64962b88272c801e9fc1862ab3e6c402d3cd
Naomi-Rhames <naomirhames2020@gmail.com>
2022-01-21 21:14:32 -0600

    added action with added gems/ uncommented CORS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.new(user_params)
+        if user.save
+           render json: user
+        else
+        render json: {error: user.errors.full_messages},  status: :unprocessable_entity
+        end
+    end
+
+    
+
+    private
+    def user_params
+        params.permit(:email, :username, :bio, :image_url, :password)
+    end
     
 end
