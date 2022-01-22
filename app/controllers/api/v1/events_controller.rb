26d976d3fb8ded71adceb4c2370563b06e4768a2
Naomi-Rhames <naomirhames2020@gmail.com>
2022-01-21 22:10:23 -0600

    deleted unnecessary code

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,6 +1,6 @@
 module Api
     module V1
-        class CommentsController < ApplicationController
+        class EventsController < ApplicationController
             
         end
     end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,21 @@
 module Api
     module V1
-        class UsersController < ApplicationController
-            
-
-
-
-
-
-
+        class UsersController < ApplicationController 
+            def create
+                user = User.new(user_params)
+                if user.save
+                    render :json user
+                else
+                render json: {error: user.errors.full_messages},  status: 403
+                end
+            end
+        
+        
+            private
+        
+            def user_params
+                params.permit(:email, :username, :bio, :password)
+            end
         end
     end
 end
